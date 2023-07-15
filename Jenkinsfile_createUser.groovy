9c035e15d73a3fe66d4ceb8e3e7b0eca165de6cf
Seth0 <Seth008@verizon.net>
2023-07-15 14:45:01 -0400

    Adding initial files

--- Jenkinsfile_createUser.groovy
+++ Jenkinsfile_createUser.groovy
@@ -13,7 +13,7 @@ pipeline {
         stage('get SF Token') {
             steps {
                 withCredentials([usernamePassword(credentialsId: "${params.Credentials}", usernameVariable: 'myUserName', passwordVariable: 'myPassword')]) {
-                    env.token = sh(script: 'node ./Scripts/NodeJS/middleware/authorization/authorization.mjs')
+                    token = sh(script: 'node ./Scripts/NodeJS/middleware/authorization/authorization.mjs')
 
                 }
             }
