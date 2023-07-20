8f59bc3f1f34f99511a7c170201e4c49504dfb4a
Seth0 <Seth008@verizon.net>
2023-07-20 19:10:27 -0400

    Adding initial files

--- Scripts/NodeJS/createUsers_fetch.mjs
+++ Scripts/NodeJS/createUsers_fetch.mjs
@@ -15,7 +15,7 @@ const main = async() => {
         "Authorization": token
     }
 
-    const url = configVars.sfDomain+'/data/api-management/apis/v1/createusers?id='+id+'&version=1.0.0&coll=homework';
+    const url = sfURL+'/data/api-management/apis/v1/createusers?id='+id+'&version=1.0.0&coll=homework';
     const data = {
         "salesforceOrgApiUrl": sfURL,
         "accessToken": token
